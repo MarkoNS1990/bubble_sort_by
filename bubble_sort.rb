75caf455f1842482a7058f8a880c7c0ebfc44a4c
Marko Zecevic <marczecevic@gmail.com>
2021-03-17 09:45:06 +0100

    fix linter

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,21 +1,21 @@
 def bubble_sort_by(arr)
-    return arr if arr.size <= 1
-  
-    swapped = true
-  
-    while swapped
-      swapped = false
-      puts arr
-      0.upto(arr.size - 2) do |i| # loop thru first el to second to last
-        if yield(arr[i], arr[i + 1]).positive?
-          arr[i], arr[i + 1] = arr[i + 1], arr[i] # swap places if first is bigger
-          swapped = true
-        end
+  return arr if arr.size <= 1
+
+  swapped = true
+
+  while swapped
+    swapped = false
+    puts arr
+    0.upto(arr.size - 2) do |i| # loop thru first el to second to last
+      if yield(arr[i], arr[i + 1]).positive?
+        arr[i], arr[i + 1] = arr[i + 1], arr[i] # swap places if first is bigger
+        swapped = true
       end
     end
-    arr
   end
-  
-  bubble_sort_by(%w[hi hello hey]) do |left, right|
-    left.length - right.length
-  end
+  arr
+end
+
+bubble_sort_by(%w[hi hello hey]) do |left, right|
+  left.length - right.length
+end
