17719c6789b2cb2222aecf8e475e8ba0d5ad8cc9
Marko Zecevic <marczecevic@gmail.com>
2021-03-17 10:17:15 +0100

    add bubble_sort method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,19 @@
+def bubble_sort(array)
+  swapped = false
+  until swapped
+    swapped = true
+    (0...array.length - 1).each do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped = false
+      end
+    end
+  end
+  array
+end
+
+puts bubble_sort([5, 6, 2, 10, 9])
+
 def bubble_sort_by(arr)
   return arr if arr.size <= 1
 
